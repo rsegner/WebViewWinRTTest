b31033c53909d8aaec121d2bd9c88a93e6feb0f0
Robert Segner <robert.segner@leapdev.io>
2022-09-14 19:38:14 +1000

    inital commit

---
+++ Services/MyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Services
+{
+    public sealed class MyService
+    {
+        public IAsyncOperation<string> GetHelloWorld() => _getHelloWorld().AsAsyncOperation();
+
+        private async Task<string> _getHelloWorld()
+        {
+            await Task.Delay(1000);
+            return $"Hello World!";
+        }
+    }
+}
---
+++ WebViewWinRTTest/WebViewWinRTTest/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Threading.Tasks;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WebViewWinRTTest
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            _ = this.InitWebView();
+        }
+
+        public async Task InitWebView()
+        {
+            await this.webview.EnsureCoreWebView2Async();
+
+            this.webview.Source = new Uri("https://google.com");
+
+            var myservice = new Services.MyService();
+            var dispatchAdapter = new WinRTAdapter.DispatchAdapter();
+
+            webview.CoreWebView2.AddHostObjectToScript("Services", dispatchAdapter.WrapObject(myservice, dispatchAdapter));
+            //webview.CoreWebView2.AddHostObjectToScript("Services", dispatchAdapter.WrapNamedObject("Services.MyService", dispatchAdapter));
+        }
+    }
+}
